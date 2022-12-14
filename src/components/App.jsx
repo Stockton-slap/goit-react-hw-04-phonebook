ae875335e22ea55e3e5daad04933f167cac71362
Andrey Yurchenko <spree92spree92@gmail.com>
2022-12-14 14:36:47 +0200

    complete hw phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,22 +7,22 @@ import ContactList from './ContactList';
 
 import { Container, PhonebookTitle, ContactsTitle } from './App.styled';
 
-export const App = () => {
-  const stateContacts = [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ];
+const stateContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
 
+export const App = () => {
   const [contacts, setContacts] = useState(
-    JSON.parse(window.localStorage.getItem('contacts'))
+    JSON.parse(window.localStorage.getItem('contacts')) || stateContacts
   );
   const [filter, setFilter] = useState('');
 
   useEffect(() => {
     window.localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts, stateContacts]);
+  }, [contacts]);
 
   const addContact = (name, number) => {
     const newContact = {
